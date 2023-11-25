bb08d068cb9d8f27d7b4d5e6c0a6c079d77b4e9e
Rt0x <144723959+Rt0x@users.noreply.github.com>
2023-11-26 01:27:31 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['email']) && isset($_POST['message'])) {
-    $to = "your_email@example.com"; // Замените на вашу электронную почту
+    $to = "nik.fomenko005@mail.ru"; // Замените на вашу электронную почту
     $subject = "Новое сообщение с сайта";
     $email = $_POST['email'];
     $message = $_POST['message'];
